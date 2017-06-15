14a7e44d21d0c64138d7fa317b4f139b5b0cc1f9
iapYang <yklorse@gmail.com>
2017-06-15 15:52:38 +0800

    can logtime runable

--- src/taobao.js
+++ src/taobao.js
@@ -1,16 +1,15 @@
-const $ = require('jquery');
-const Promise = require('bluebird');
+const Nightmare = require('nightmare');
 
-const nightmare = require('nightmare')({
-    Promise,
+const login = new Nightmare({
     openDevTools: {
         mode: 'detach',
     },
     show: true,
     waitTimeout: 30 * 60 * 1000,
+    dock: true,
 });
 
-nightmare
+login
     .viewport(1280, 800)
     .goto('https://detail.tmall.com/item.htm?id=526006447112&spm=a21bz.7725273.1998564503.1.MhskeM&umpChannel=qianggou&u_channel=qianggou&sku_properties=1627207:13999134')
     // .wait('.J_TSaleProp li:nth-child(4) a')
@@ -20,27 +19,23 @@ nightmare
     .wait('a.sn-login')
     .click('a.sn-login')
     // for login wait qr scan
-    .wait(30 * 1000)
-    .catch(error => {
-        console.error('Search failed:', error);
+    .wait(30 * 1000);
+
+login
+    // .wait()
+    .refresh()
+    .wait('body');
+
+const show = false;
+
+login.exists('.J_LinkBuy')
+    .then(text => {
+        console.log(text);
     });
 
-function loop() {
-    let ifLinkBuyExists = false;
-    nightmare
-        .exists('.J_LinkBuy')
-        .then(ifExists => {
-            console.log('in');
-            ifLinkBuyExists = ifExists;
-        });
-
-    if (ifLinkBuyExists) {
-        console.log('exists');
-    } else {
-        console.log('not exists');
-        nightmare
-            .refresh()
-            .wait('body');
-        loop();
-    }
-}
+login
+    .wait()
+    .refresh()
+    .wait('body')
+    .end(() => 'some value');
+// loop();
